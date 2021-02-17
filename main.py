b2587246e96fd130f51d6c8f086cf2064d77d5ef
Yash R <crathody007@gmail.com>
2021-02-17 07:53:44 +0000

    Changed print statement

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Yash. Success!'
+    return 'Hello Yash, Finally Success!'
 
 
 if __name__ == '__main__':
